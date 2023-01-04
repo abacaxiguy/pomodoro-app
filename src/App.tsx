cdf0132a2137425bedbbab45c694de6b7af55f2d
João Lucas <Jlucasoc@hotmail.com>
2023-01-03 23:27:29 -0300

    Creating useInterval hook 🎣

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { PomodoroTimer } from "./components/pomodoro-timer";
 
 function App(): JSX.Element {
-    return <div className="App">sup bitches</div>;
+    return (
+        <div className="App">
+            <PomodoroTimer defaultPomodoroTime={1500} />
+        </div>
+    );
 }
 
 export default App;
